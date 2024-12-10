b3ab0610290692522a7eb0ed9b7330d508007939
Bertie Raffle <bertierwork@gmail.com>
2024-12-10 16:34:38 +0000

    add suspense

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Modelviewer from "@/components/modelviewer";
-import React from "react";
+import React, { Suspense } from "react";
 
 export default function MainPage() {
   return (
     <div className="bg-stone-500 p-10 border-2 border-red-800 rounded-2xl w-1/4 mx-auto my-10">
-      <Modelviewer src="/earth_globe_hologram_glb.glb" />
+      <Suspense fallback={<p>This is a fallback</p>}>
+        <Modelviewer src="/earth_globe_hologram_glb.glb" />
+      </Suspense>
     </div>
   );
 }
